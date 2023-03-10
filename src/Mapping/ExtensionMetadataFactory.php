2029c83a5d5f5e6d0d134f4332f75805b10d9a39
Gaël Reyrol <gael@mention.com>
2023-03-10 14:43:42 +0100

    fix: use isTransient to assert that class should have been

--- src/Mapping/ExtensionMetadataFactory.php
+++ src/Mapping/ExtensionMetadataFactory.php
@@ -93,7 +93,7 @@ class ExtensionMetadataFactory
         if (null !== $meta->reflClass) {
             foreach (array_reverse(class_parents($meta->getName())) as $parentClass) {
                 // read only inherited mapped classes
-                if ($cmf->hasMetadataFor($parentClass)) {
+                if ($cmf->hasMetadataFor($parentClass) || !$cmf->isTransient($parentClass)) {
                     $class = $this->objectManager->getClassMetadata($parentClass);
                     $this->driver->readExtendedMetadata($class, $config);
                     $isBaseInheritanceLevel = !$class->isInheritanceTypeNone()
